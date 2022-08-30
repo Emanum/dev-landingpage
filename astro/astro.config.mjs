bbc957338884ddc45045a5c99acfb7dcc2e292fa
Manuel Eiweck <manuel.eiweck@bkf.at>
2022-08-31 00:08:19 +0200

    Add animated Name to index.astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()]
+  integrations: [react(), preact(), mdx()]
 });
